cf1e1e2e75f0afe5c7bd5401630d971c24d1e532
vyouzhi <quan.peng@pixitek.com>
2013-11-25 18:37:35 +0800

    hashtable

---
+++ hashtable/hashtab.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hashtab.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/25/2013 06:00:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vyouzhi (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#ifndef hashtab_H
+#define hashtab_H
+#ifdef __cplusplus
+ extern "C"
+ {
+#endif
+
+
+#ifdef __cplusplus
+ }
+#endif
+#endif /* --- #hashtab_H ---*/
+ /* vim: set ts=4 sw=4: */
+
