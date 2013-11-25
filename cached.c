024f28d4376ea7f82b7d70ca1a69da89cd80a124
vyouzhi <quan.peng@pixitek.com>
2013-11-25 17:34:14 +0800

    实现一个hashtable

--- cached.c
+++ cached.c
@@ -27,24 +27,9 @@
 void cached_init (int shms){
 
     cache_shmid = shmget(IPC_PRIVATE, shms*sizeof(CQI), SHM_R | SHM_W);
-    if(cache_shmid == NULL) exit(-1);
+    if(cache_shmid == -1) exit(-1);
 
-    token_efd = eventfd(0, 0);
-    if(token_efd == -1) exit(1);
-
-    cache_hash_table = hcreate(8);                      /* create hash table */
-
-    /* Listen for notifications from other threads */
-    event_set(&token_process->notify_event, token_efd,
-              EV_READ | EV_PERSIST, cached_accept, token_process);
-    event_base_set(token_process->base, &token_process->notify_event);
-    if (event_add(&token_process->notify_event, 0) == -1) {
-        d_log("Can't monitor libevent notify pipe");
-        exit(1);
-    }   
-
-    event_base_loop(token_process->base, 0);
-
+    
 }		/* -----  end of function cached_init  ----- */
 
 
@@ -56,7 +41,7 @@ void cached_init (int shms){
  * =====================================================================================
  */
 void cached_accept (int fd, short ev, void *arg){
-    return <+return_value+>;
+  //  return <+return_value+>;
 }		/* -----  end of function cached_accept  ----- */
  /* vim: set ts=4 sw=4: */
 
