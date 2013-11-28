160f8e63479177e562146d79c4aa734e1e9a0e66
vyouzhi <quan.peng@pixitek.com>
2013-11-28 18:15:10 +0800

    hashtable

--- config_global.h
+++ config_global.h
@@ -22,13 +22,85 @@
  {
 #endif
 
+#ifndef STANDARD
+# define STANDARD
+# ifndef STDIO
+#  include <stdio.h>
+#  define STDIO
+# endif
+# ifndef STDDEF
+#  include <stddef.h>
+#  define STDDEF
+# endif
+#include <sys/types.h>
+
+#include "conf_lib.h"
+
+typedef  unsigned long long  ub8;
+#define UB8MAXVAL 0xffffffffffffffffLL
+#define UB8BITS 64
+typedef    signed long long  sb8;
+#define SB8MAXVAL 0x7fffffffffffffffLL
+typedef  unsigned long  int  ub4;   /* unsigned 4-byte quantities */
+#define UB4MAXVAL 0xffffffff
+typedef    signed long  int  sb4;
+#define UB4BITS 32
+#define SB4MAXVAL 0x7fffffff
+typedef  unsigned short int  ub2;
+#define UB2MAXVAL 0xffff
+#define UB2BITS 16
+typedef    signed short int  sb2;
+#define SB2MAXVAL 0x7fff
+typedef  unsigned       char ub1;
+#define UB1MAXVAL 0xff
+#define UB1BITS 8
+typedef    signed       char sb1;   /* signed 1-byte quantities */
+#define SB1MAXVAL 0x7f
+typedef                 int  word;  /* fastest type available */
+
+#define bis(target,mask)  ((target) |=  (mask))
+#define bic(target,mask)  ((target) &= ~(mask))
+#define bit(target,mask)  ((target) &   (mask))
+#ifndef min
+# define min(a,b) (((a)<(b)) ? (a) : (b))
+#endif /* min */
+#ifndef max
+# define max(a,b) (((a)<(b)) ? (b) : (a))
+#endif /* max */
+#ifndef align
+# define align(a) (((ub4)a+(sizeof(void *)-1))&(~(sizeof(void *)-1)))
+#endif /* align */
+#ifndef abs
+# define abs(a)   (((a)>0) ? (a) : -(a))
+#endif
+#define TRUE  1
+#define FALSE 0
+#define SUCCESS 0  /* 1 on VAX */
+
+
 typedef struct _conn conn;
 struct _conn{
-    char *listen_ip;
-    ssize_t format_len;
-    MSGFORMAT *next;
+    char *server_ip;   /* server listen ip */
+    ssize_t server_port; /* server listen port */
+
+    ssize_t unix_sock; /* is socket */
+    char *path;   /* socket path */
+
+    char *pg_host;  /* db host */
+    ssize_t pg_port;  /* db port */
+
+    ssize_t do_daemonize; /* is daemon */
+
+    char *pid_file;  /* pid path */
+    
+    ssize_t process_num;  /* work process number */
+
+    float factor;
 };
 
+conn *conn_global;
+
+void conn_init_global();
 
 #ifdef __cplusplus
  }
